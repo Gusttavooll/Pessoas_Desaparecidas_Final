3ca8feb2ae037ea2072853b4d1cf44e549a74d5e
GustavoAlves <gustavooficial2004@outlook.com>
2025-05-22 14:41:09 -0300

    Add Alteraçoes em services

--- src/services/localizacao.service.js
+++ src/services/localizacao.service.js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../generated/prisma/index.js';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 import { v4 as uuidv4 } from 'uuid';
